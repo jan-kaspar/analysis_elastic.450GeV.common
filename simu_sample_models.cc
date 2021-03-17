c590ea08ea98c7ef67fdefc7ff22c930d1d19be9
Jan Kaspar <jan.kaspar@cern.ch>
2021-03-17 15:01:57 +0100

    import model sampling code

--- simu_sample_models.cc
+++ simu_sample_models.cc
@@ -1,5 +1,13 @@
 #include "classes/command_line_tools.hh"
 
+#include "Elegent/Constants.h"
+#include "Elegent/CoulombInterference.h"
+
+#include "classes/HadronicFitModel.hh"
+
+#include "TFile.h"
+#include "TGraph.h"
+
 #include <cstring>
 
 using namespace std;
@@ -11,6 +19,46 @@ void PrintUsage()
 	printf("USAGE: program <option> <option>\n");
 	printf("OPTIONS:\n");
 	//printf("    -cfg <file>       config file\n");
+}
+
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
+
+void SampleModel(Model *m, const string &label)
+{
+	model = m;
+
+	TDirectory *d_top = gDirectory;
+
+	gDirectory = d_top->mkdir(label.c_str());
+
+	TGraph *g_dsdt_H = new TGraph();
+	TGraph *g_dsdt_CH = new TGraph();
+
+	for (double mt = 1E-4; mt <= 0.11; )
+	{
+		coulomb->mode = CoulombInterference::mPH;
+		TComplex F_H = coulomb->Amp(-mt);
+
+		coulomb->mode = CoulombInterference::mKL;
+		TComplex F_CH = coulomb->Amp(-mt);
+
+		int idx = g_dsdt_H->GetN();
+		g_dsdt_H->SetPoint(idx, mt, cnts->sig_fac * F_H.Rho2());
+		g_dsdt_CH->SetPoint(idx, mt, cnts->sig_fac * F_CH.Rho2());
+
+		double dmt = 5E-3;
+		if (mt < 0.10) dmt = 1E-3;
+		if (mt < 0.004) dmt = 1E-4;
+		if (mt < 0.001) dmt = 1E-5;
+		mt += dmt;
+	}
+
+	g_dsdt_H->Write("g_dsdt_H");
+	g_dsdt_CH->Write("g_dsdt_CH");
+
+	gDirectory = d_top;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -41,7 +89,42 @@ int main(int argc, const char **argv)
 		return 1;
 	}
 
-	// TODO
+	// init Elegent
+	Constants::Init(2*450, cnts->mPP);
+    cnts->Print();
+
+	coulomb->ffType = coulomb->ffPuckett;
+	coulomb->Print();
+
+	// prepare output
+	TFile *f_out = TFile::Open("models.root", "recreate");
+
+	// sample models
+	HadronicFitModel *hfm = new HadronicFitModel();
+
+	coulomb->mode = CoulombInterference::mKL;
+
+	hfm->a = 5.67E6;
+	hfm->b1 = 8.5;
+	hfm->b2 = 0.;
+	hfm->b3 = 0.;
+
+	hfm->t1 = 0.2;
+	hfm->t2 = 0.5;
+
+	hfm->phaseMode = HadronicFitModel::pmConstant;
+
+	hfm->p0 = M_PI/2. - atan(0.05);
+	SampleModel(hfm, "exp1_con_rho0.05");
+
+	hfm->p0 = M_PI/2. - atan(0.10);
+	SampleModel(hfm, "exp1_con_rho0.10");
+
+	hfm->p0 = M_PI/2. - atan(0.15);
+	SampleModel(hfm, "exp1_con_rho0.15");
+
+	// clean up
+	delete f_out;
 
 	return 0;
 }
