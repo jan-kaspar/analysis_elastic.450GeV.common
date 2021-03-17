d5a818dd9afd6ef646e43b5279d95da89b99200b
Jan Kaspar <jan.kaspar@cern.ch>
2021-03-17 10:48:54 +0100

    add min/max seed parameters

--- simu_process_fits.cc
+++ simu_process_fits.cc
@@ -10,7 +10,8 @@ void PrintUsage()
 {
 	printf("USAGE: program <option> <option>\n");
 	printf("OPTIONS:\n");
-	//printf("    -cfg <file>       config file\n");
+	printf("    -seed-min <integer>        first seed to process\n");
+	printf("    -seed-max <integer>        last seed to process\n");
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -18,7 +19,8 @@ void PrintUsage()
 int main(int argc, const char **argv)
 {
 	// defaults
-	//string cfg_file = "config.py";
+	unsigned int seed_min = 0;
+	unsigned int seed_max = 0;
 
 	// parse command line
 	for (int argi = 1; (argi < argc) && (cl_error == 0); ++argi)
@@ -29,7 +31,8 @@ int main(int argc, const char **argv)
 			continue;
 		}
 
-		//if (TestStringParameter(argc, argv, argi, "-cfg", cfg_file)) continue;
+		if (TestUIntParameter(argc, argv, argi, "-seed-min", seed_min)) continue;
+		if (TestUIntParameter(argc, argv, argi, "-seed-max", seed_max)) continue;
 
 		printf("ERROR: unknown option '%s'.\n", argv[argi]);
 		cl_error = 1;
