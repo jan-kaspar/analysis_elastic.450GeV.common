217b0c0b50020403c7b6b2d54c504d6e2de0173e
Jan Kaspar <jan.kaspar@cern.ch>
2021-03-17 12:20:42 +0100

    first implementation of input import

--- input_import.cc
+++ input_import.cc
@@ -1,6 +1,12 @@
+#include <TDirectory.h>
 #include "classes/command_line_tools.hh"
 
+#include "TFile.h"
+
+#include <cstdio>
 #include <cstring>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -18,7 +24,9 @@ void PrintUsage()
 int main(int argc, const char **argv)
 {
 	// defaults
-	//string cfg_file = "config.py";
+	string dir_low_beta = "";
+	string dir_high_beta = "";
+	string binnings_list = "";
 
 	// parse command line
 	for (int argi = 1; (argi < argc) && (cl_error == 0); ++argi)
@@ -29,7 +37,9 @@ int main(int argc, const char **argv)
 			continue;
 		}
 
-		//if (TestStringParameter(argc, argv, argi, "-cfg", cfg_file)) continue;
+		if (TestStringParameter(argc, argv, argi, "-dir-low-beta", dir_low_beta)) continue;
+		if (TestStringParameter(argc, argv, argi, "-dir-high-beta", dir_high_beta)) continue;
+		if (TestStringParameter(argc, argv, argi, "-binnings", binnings_list)) continue;
 
 		printf("ERROR: unknown option '%s'.\n", argv[argi]);
 		cl_error = 1;
@@ -41,7 +51,63 @@ int main(int argc, const char **argv)
 		return 1;
 	}
 
-	// TODO
+	// validate input
+	if (dir_low_beta.empty() || dir_high_beta.empty() || binnings_list.empty())
+	{
+		printf("ERROR: insufficient input\n");
+		return 2;
+	}
+
+	// parse list of binnings
+	vector<string> binnings;
+
+	{
+		stringstream ss(binnings_list);
+		string binning;
+		while (getline(ss, binning, ','))
+			binnings.push_back(binning);
+	}
+
+	// open files
+	TFile *f_hist_low_beta = TFile::Open((dir_low_beta + "/data/merged.root").c_str());
+	TFile *f_unc_low_beta = TFile::Open((dir_low_beta + "/studies/systematics/matrix.root").c_str());
+
+	TFile *f_hist_high_beta = TFile::Open((dir_high_beta + "/data/merged.root").c_str());
+	TFile *f_unc_high_beta = TFile::Open((dir_high_beta + "/studies/systematics/matrix.root").c_str());
+
+	TFile *f_out = TFile::Open("import.root", "recreate");
+
+	if (!f_hist_low_beta || !f_unc_low_beta || !f_hist_high_beta || !f_unc_high_beta || !f_out)
+	{
+		printf("ERROR: some files cannot be opened.\n");
+		return 3;
+	}
+
+	for (const auto &binning : binnings)
+	{
+		printf("* %s\n", binning.c_str());
+
+		TDirectory *d_binning = f_out->mkdir(binning.c_str());
+
+		gDirectory = d_binning->mkdir("low_beta");
+		f_hist_low_beta->Get((binning + "/merged/combined/h_dsdt").c_str())->Write("h_dsdt_cen_stat");
+		f_unc_low_beta->Get(("matrices/all-but-norm/" + binning + "/cov_mat").c_str())->Write("m_dsdt_rel_syst");
+		f_unc_low_beta->Get(("matrices/all-but-norm/" + binning + "/h_stddev").c_str())->Write("h_dsdt_rel_syst");
+
+		gDirectory = d_binning->mkdir("high_beta");
+		f_hist_high_beta->Get((binning + "/merged/combined/h_dsdt").c_str())->Write("h_dsdt_cen_stat");
+		f_unc_high_beta->Get(("matrices/all-but-norm/" + binning + "/cov_mat").c_str())->Write("m_dsdt_rel_syst");
+		f_unc_high_beta->Get(("matrices/all-but-norm/" + binning + "/h_stddev").c_str())->Write("h_dsdt_rel_syst");
+	}
+
+	// clean up
+	delete f_hist_low_beta;
+	delete f_unc_low_beta;
+
+	delete f_hist_high_beta;
+	delete f_unc_high_beta;
+
+	delete f_out;
 
 	return 0;
 }
