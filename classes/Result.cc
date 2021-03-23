87f6f59790d19a5aef1a877fa07c8d51d014cb30
Jan Kaspar <jan.kaspar@cern.ch>
2021-03-23 18:01:36 +0100

    add ROOT import/export

--- classes/Result.cc
+++ classes/Result.cc
@@ -57,3 +57,29 @@ void Result::Write(const std::string &file) const
 
 	f_out.close();
 }
+
+//----------------------------------------------------------------------------------------------------
+
+std::unique_ptr<TH1D> Result::ExportROOT() const
+{
+	unique_ptr<TH1D> h(new TH1D("", "", data.size(), 0., data.size()));
+
+	int bi = 0;
+	for (const auto &it : data)
+	{
+		bi++;
+
+		h->GetXaxis()->SetBinLabel(bi, it.first.c_str());
+		h->SetBinContent(bi, it.second);
+	}
+
+	return h;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Result::ImportROOT(const TH1D *h)
+{
+	for (int bi = 1; bi <= h->GetNbinsX(); ++bi)
+		data[h->GetXaxis()->GetBinLabel(bi)] = h->GetBinContent(bi);
+}
--- classes/Result.hh
+++ classes/Result.hh
@@ -1,9 +1,11 @@
 #ifndef _Result_hh_
 #define _Result_hh_
 
+#include "TH1D.h"
+
 #include <map>
+#include <memory>
 #include <string>
-#include <cmath>
 
 //----------------------------------------------------------------------------------------------------
 
@@ -11,11 +13,17 @@ struct Result
 {
 	std::map<std::string, double> data;
 
-	double Get(const std::string &name, double default_value = NAN) const;
+	Result() {}
+	Result(const TH1D *h) { ImportROOT(h); }
+
+	double Get(const std::string &name, double default_value = -1) const;
 	void Set(const std::string &name, double value);
 
 	void Load(const std::string &file);
 	void Write(const std::string &file) const;
+
+	std::unique_ptr<TH1D> ExportROOT() const;
+	void ImportROOT(const TH1D *h);
 };
 
 #endif
